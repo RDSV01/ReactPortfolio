0f218b7d26f5eae4d0c63daa5b742e3d832cfb54
unknown <raphds76@gmail.com>
2023-08-04 22:52:18 +0200

    Hamburger menu + scrollbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,80 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import React, { useState } from "react";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsChecked(false);
+  };
+  const handleMenuButtonClick = (event) => {
+    event.preventDefault(); // Empêche le comportement par défaut du clic sur le bouton
+    setIsChecked(!isChecked);
+  };
   return (
-    <div className="navbar">
-      <ul>
-        <a href="#">
-          <li>Accueil</li>
-        </a>
-        <a href="#projets">
-          <li>Projets</li>
-        </a>
-        <a href="#competences">
-          <li>Compétences</li>
-        </a>
-        <a href="#">
-          <li>Experience</li>
-        </a>
-      </ul>
+    <div className="navigation">
+      <div className="navbar">
+        <ul>
+          <a href="#">
+            <li>Accueil</li>
+          </a>
+          <a href="#projets">
+            <li>Projets</li>
+          </a>
+          <a href="#competences">
+            <li>Compétences</li>
+          </a>
+          <a href="#">
+            <li>Experience</li>
+          </a>
+        </ul>
+      </div>
+      <div className="hamburger-menu">
+        <input
+          id="menu__toggle"
+          type="checkbox"
+          checked={isChecked}
+          onChange={() => setIsChecked(!isChecked)}
+        />
+        <label
+          className="menu__btn"
+          htmlFor="menu__toggle"
+          onClick={handleMenuButtonClick}
+        >
+          <span></span>
+        </label>
+
+        <ul className="menu__box">
+          <li>
+            <a className="menu__item" href="#" onClick={handleLinkClick}>
+              Accueil
+            </a>
+          </li>
+          <li>
+            <a className="menu__item" href="#projets" onClick={handleLinkClick}>
+              Projets
+            </a>
+          </li>
+          <li>
+            <a
+              className="menu__item"
+              href="#competences"
+              onClick={handleLinkClick}
+            >
+              Compétences
+            </a>
+          </li>
+          <li>
+            <a
+              className="menu__item"
+              href="#experience"
+              onClick={handleLinkClick}
+            >
+              Experience
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
